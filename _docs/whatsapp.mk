fd56bb7e6d17941b1ebb8b0bb001dc2ea02a3d2f
chen-si-tong <128065639+chen-si-tong@users.noreply.github.com>
2023-03-20 12:11:07 +0100

    Create whatsapp.mk

---
+++ _docs/whatsapp.mk
@@ -0,0 +1,17 @@
+---
+title: Whatsapp
+category: Contact Syncing
+order: 1
+---
+
+Sync your Whatsapp  contacts with
+
+> Signing up with Facebook automatically starts syncing contacts.
+
+To sync your contacts:
+
+1. Open your *User Settings*
+2. Select the **Connect Facebook** button
+3. Authorise ChatApp
+
+![](//placehold.it/800x600)
